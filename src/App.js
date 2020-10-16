d7ddd097459eb677ac6c7f076372f500cf45de7e
ADI-Netizen <71365604+ADI-Netizen@users.noreply.github.com>
2020-10-16 12:27:11 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Segmen1 from './component/Segmen1';
+import Segmen2 from './component/Segmen2';
+
+function App() {
+  return (
+    <>
+      <Segmen1 />
+      <Segmen2 />
+    </>
+  );
+}
+
+export default App;
